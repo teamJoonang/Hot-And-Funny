4c98ae1d0fe293340f5914aafcf6bebf7c057a76
il3ong <bbonga0609@gmail.com>
2023-09-03 21:21:28 +0900

    2th calendar add eventDate & modify result size

--- ticket/static/purchase.js
+++ ticket/static/purchase.js
@@ -1,5 +1,22 @@
 let date = new Date();
+const concertSite = document.getElementById('concert-site');
+const concertTime = document.getElementById('concert-time');
+const concertDate = document.getElementById('concert-date');
+const concertGrade = document.getElementById('concert-grade');
 let selectedDate = null;
+const concertDates = [1, 2, 3, 5, 6, 7, 8, 9, 10];
+const clickableDates = generateClickableDates(concertDates);
+
+function generateClickableDates(concertDates) {
+    const clickableDates = {};
+
+    for (const date of concertDates) {
+        clickableDates[date] = true;
+    }
+
+    return clickableDates;
+}
+
 
 const renderCalendar = () =>{
     const viewYear = date.getFullYear();
@@ -50,26 +67,26 @@ const renderCalendar = () =>{
     });
 
     document.querySelector('.dates').innerHTML = dates.join('');
-
-
+ 
     todayCheck();
+    concertCheck();
+
+    //  오늘 날짜 그리기
     function todayCheck(){
-        //  오늘 날짜 그리기
         const today = new Date();
         if (viewMonth === today.getMonth() && viewYear === today.getFullYear()){
             for (let date of document.querySelectorAll('.this')) {
                 if (+date.innerText === today.getDate()){
                     date.setAttribute('id','today');
-                    break;
+                    // break
                 }
             }
         }
     }
 
-    concertCheck();
+
     // 날짜에 이벤트 표시하기
     function concertCheck(){
-        const concertDates = [1, 2, 3, 5, 6, 7, 8, 9, 10];
         const concertMonth = new Date();
         if (viewMonth === concertMonth.getMonth() && viewYear === concertMonth.getFullYear()){
             const concertElements = document.querySelectorAll(`.this`);
@@ -81,31 +98,21 @@ const renderCalendar = () =>{
                         dates.classList.add(`concert`);
                     }
             }
-
+        
         }
     }
-
-    //  // 클릭한 날짜 표시하기
-    // if (selectedDate !== null) {
-    //     const selectedElement = document.querySelector(`.date span.this:nth-child(${selectedDate})`);
-    // if (selectedElement) {
-    //     selectedElement.classList.add('selected');
-    // }
+    //  날짜 클릭 시 출력
+    document.querySelector('.dates').addEventListener('click', (event) => {
+        const clickedDate = event.target.innerText;
+        const [clickedDay] = clickedDate.split('\n');
+    
+        if (clickedDay && clickableDates[Number(clickedDay)]) {
+            const formattedDay = String(clickedDay).padStart(2, '0'); // 두 자리로 포맷팅
+            concertDate.innerHTML = `• ${viewYear}-${String(viewMonth + 1).padStart(2, '0')}-${formattedDay}`;
+        }
+    });
 };
-
-
-// document.querySelector('.dates').addEventListener('click', (event) => {
-//     const clickedElement = event.target;
-//     if (clickedElement.classList.contains('this')) {
-//         if (selectedDate !== null) {
-//             const prevSelectedElement = document.querySelector(`.date span.this:nth-child(${selectedDate})`);
-//             prevSelectedElement.classList.remove('selected');
-//         }
-//         selectedDate = +clickedElement.innerText;
-//         clickedElement.classList.add('selected');
-//     }
-// });
-
+    
 renderCalendar();
 
 const prevMonth = () => {
@@ -124,4 +131,7 @@ const goToday = () => {
     date = new Date();
     renderCalendar();
 };
+
+
+
 
