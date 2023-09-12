d4faed98a42d62cf62d9af086bf6dff4fb9d074a
il3ong <bbonga0609@gmail.com>
2023-09-12 12:44:54 +0900

    ignore 확인중

---
+++ src/main/java/com/choongang/concert/dto/ticket/ConcertInfoDto.java
@@ -0,0 +1,11 @@
+package com.choongang.concert.dto.ticket;
+
+import lombok.Data;
+
+@Data
+
+public class ConcertInfoDto {
+    private String concertName;
+    private String concertPlace;
+    private String formattedTime;
+}
---
+++ src/test/java/com/choongang/concert/ConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choongang.concert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
