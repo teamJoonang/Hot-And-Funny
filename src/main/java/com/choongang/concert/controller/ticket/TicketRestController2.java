67058a0c5e75f2de4334a8fa72347bd73477246e
il3ong <bbonga0609@gmail.com>
2023-09-12 21:45:20 +0900

    현재까지 한거 정리함 현욱이 겹치는거 다삭제

--- src/main/java/com/choongang/concert/controller/ticket/TicketRestController2.java
+++ src/main/java/com/choongang/concert/controller/ticket/TicketRestController2.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.choongang.concert.dto.ticket.ChoiceDateDto;
-import com.choongang.concert.service.ticket.TicketService;
+import com.choongang.concert.service.ticket.TicketService2;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -20,14 +20,8 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/ticket")
 public class TicketRestController2 {
 
-	private final TicketService ticketService;
+	private final TicketService2 ticketService;
 
-	// 실제 좌석번호 매핑용 json 데이터 js 사용 api 컨트롤러
-	@GetMapping("/seat/choice/{concertDate}/{seatIndex}")
-	public String OriginSeatAPI(@PathVariable String concertDate, @PathVariable int seatIndex) {
-		String originSeatNumber = ticketService.originSeatMapping(seatIndex);
-		return originSeatNumber;
-	}
 
 	@GetMapping("/home/calendar/{concertId}")
 	public ResponseEntity<List<ChoiceDateDto>> seat(@PathVariable String concertId) {
