41d48b659f000e4ece1369a9492167e156f41c8f
HyunSense <hyunsense1022@gmail.com>
2023-09-17 23:28:07 +0900

    Add. header_index.html

--- src/main/java/com/choongang/concert/controller/HomeController.java
+++ src/main/java/com/choongang/concert/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.choongang.concert.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-	
+
+
 	@GetMapping("/")
-	public String home() {
-		
+	public String home(Model model) {
+
+
+//		model.addAttribute()
 		return "index";
 		
 	}
